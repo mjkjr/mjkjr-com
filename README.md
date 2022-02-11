a6485f00b291e16b4480d2df53ff9a7558aa939b
Michael J. Kozubal Jr <mjkjr84@gmail.com>
2022-02-11 12:06:47 -0500

    Added mailto link obfuscation for spam avoidance, corrected bg

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Personal website for Michael J. Kozubal Jr.
 
 - CSS: https://tailwindcss.com
 - CSS Minifying: http://css.github.io/csso/csso.html
+- JS Obfuscation (for spam avoidance): https://obfuscator.io/
 - Icon Generation: https://realfavicongenerator.net
---
+++ js/mjkjr.js
@@ -0,0 +1,14 @@
+/* slightly obfuscated function to build mail link */
+
+function contact() {
+	var a = 'co';
+	var b = 'mjk';
+	var c = String.fromCharCode(58);
+	var d = '\u002E';
+	var e = Math.pow(2,6);
+	var m = 'm';
+	var n = 'bWlrZQ==';
+	var x = atob('bHRv');
+	var z = 'jr';
+	window.location = ("mai" + x + c + atob(n) + String.fromCharCode(e) + b + z + d + a + m);
+}
