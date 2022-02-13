e4b94710f71227f7bc142f3227f9fc6222695c8c
Michael J. Kozubal Jr <mjkjr84@gmail.com>
2022-02-13 12:45:44 -0500

    Updated email address

--- js/mjkjr.js
+++ js/mjkjr.js
@@ -2,13 +2,13 @@
 
 function contact() {
 	var a = 'co';
-	var b = 'gma';
+	var b = 'bWpr';
 	var c = String.fromCharCode(58);
 	var d = '\u002E';
 	var e = Math.pow(2,6);
 	var m = 'm';
-	var n = 'bWpranI4NA==';
+	var n = 'bWlrZQ==';
 	var x = atob('bHRv');
-	var z = 'il';
-	window.location = ("mai" + x + c + atob(n) + String.fromCharCode(e) + b + z + d + a + m);
+	var z = 'jr';
+	window.location = ("mai" + x + c + atob(n) + String.fromCharCode(e) + atob(b) + z + d + a + m);
 }
